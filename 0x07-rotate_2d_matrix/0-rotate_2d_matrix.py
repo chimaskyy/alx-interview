2302487d04387620c4b462524c7cb0d63b4a6551
chimaskyy <chiamakahj@gmail.com>
2024-06-11 14:35:01 +0100

    pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""This module rotates a 2D matrix 90 degrees clockwise"""
+
 
 def rotate_2d_matrix(matrix):
+    """Rotates a 2D matrix 90 degrees clockwise"""
     matrix[:] = [list(i) for i in zip(*matrix)]
     for row in matrix:
         row.reverse()
-    return matrix
