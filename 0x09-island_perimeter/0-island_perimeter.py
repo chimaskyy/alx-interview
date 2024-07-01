d6c35d5403e174cfefec1209b5b98b9499b97f29
chimaskyy <chiamakahj@gmail.com>
2024-07-01 04:18:33 +0100

    doc

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,21 +1,43 @@
-#!/usr/bin/python3
-"""
-This module returns the perimeter of the
-Island described in grid
-"""
 def island_perimeter(grid):
-    """function that returns the perimeter of the island"""
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            # If the current cell is land(1)
-            if grid[i][j] == 1:
-                # Add 4 to the perimeter
-                perimeter += 4
-                # If the cell to the left is land(1), subtract 2
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                # If the cell above is land, subtract 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:  # Land cell
+                # Check the cell above
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                # Check the cell below
+                if i == rows-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                # Check the cell to the left
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                # Check the cell to the right
+                if j == cols-1 or grid[i][j+1] == 0:
+                    perimeter += 1
     return perimeter
+
+# #!/usr/bin/python3
+# """
+# This module returns the perimeter of the
+# Island described in grid
+# """
+# def island_perimeter(grid):
+#     """function that returns the perimeter of the island"""
+#     perimeter = 0
+#     for i in range(len(grid)):
+#         for j in range(len(grid[i])):
+#             # If the current cell is land(1)
+#             if grid[i][j] == 1:
+#                 # Add 4 to the perimeter
+#                 perimeter += 4
+#                 # If the cell to the left is land(1), subtract 2
+#                 if i > 0 and grid[i - 1][j] == 1:
+#                     perimeter -= 2
+#                 # If the cell above is land, subtract 2
+#                 if j > 0 and grid[i][j - 1] == 1:
+#                     perimeter -= 2
+#     return perimeter
