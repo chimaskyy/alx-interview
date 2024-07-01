f3f4a4ec8b5a606d994cb96e68a74c1fe6ef41d6
chimaskyy <chiamakahj@gmail.com>
2024-07-01 04:02:10 +0100

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            # If the current cell is land(1)
+            if grid[i][j] == 1:
+                # Add 4 to the perimeter
+                perimeter += 4
+                # If the cell to the left is land(1), subtract 2
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                # If the cell above is land, subtract 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
