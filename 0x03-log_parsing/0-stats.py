a17d678b61241f4f8370bd0a1482761cf75d621f
chimaskyy <chiamakahj@gmail.com>
2024-07-13 19:37:08 +0100

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,45 @@
 #!/usr/bin/python3
-"""Script that reads stdin line by line and computes metrics:"""
 
-import re
+"""Script that reads stdin line by line and computes metrics"""
 
-fp = (
-    r'\s*(?P<ip>\S+)\s*',
-    r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-    r'\s*"(?P<request>[^"]*)"\s*',
-    r'\s*(?P<status_code>\S+)',
-    r'\s*(?P<file_size>\d+)'
-)
-pattern = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
-
-status_counts = {'200': 0,
-                 '301': 0,
-                 '400': 0,
-                 '401': 0,
-                 '403': 0,
-                 '404': 0,
-                 '405': 0,
-                 '500': 0}
-file_size = 0
-counter = 0
+import sys
 
 
-def print_statistics() -> None:
-    """routine for printing statistics"""
-    print(f"File size: {file_size}")
-    for k, v in sorted(status_counts.items()):
-        if (v > 0):
-            print(f"{k}: {v}")
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-if __name__ == '__main__':
-    try:
-        while True:
-            line = input()
-            if re.fullmatch(pattern, line):
-                line_split = line.split(" ")
-                if line_split[-2] in status_counts:
-                    status_counts[line_split[-2]] += 1
-                file_size += int(line_split[-1])
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
-            counter += 1
-            if (counter % 10 == 0):
-                print_statistics()
+count = 0
+size = 0
 
-        # Check for EOF (end of file)
-        # if counter % 10 != 0:  # If the last batch of lines is less than 10
-        #     print_statistics()
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
-    except (KeyboardInterrupt, EOFError):
-        print_statistics()
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
+
+
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
